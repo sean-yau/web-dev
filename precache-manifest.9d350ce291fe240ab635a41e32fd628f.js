f619066c82874e62510c22df6384169a0a1d0ce9
sean-yau <sean.yau@aquaq.co.uk>
2019-12-16 15:56:47 +0000

    Updates

---
+++ precache-manifest.9d350ce291fe240ab635a41e32fd628f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1220260159c70610c7b1304d8ce890e",
+    "url": "/web-dev/index.html"
+  },
+  {
+    "revision": "6bb4cd1aae46fe468d25",
+    "url": "/web-dev/static/css/2.7c660877.chunk.css"
+  },
+  {
+    "revision": "05daa733cae65f9f0f13",
+    "url": "/web-dev/static/css/main.dafae7c5.chunk.css"
+  },
+  {
+    "revision": "6bb4cd1aae46fe468d25",
+    "url": "/web-dev/static/js/2.2b8dc0ba.chunk.js"
+  },
+  {
+    "revision": "05daa733cae65f9f0f13",
+    "url": "/web-dev/static/js/main.c87c073b.chunk.js"
+  },
+  {
+    "revision": "b3345c2c7abfd44ee1f5",
+    "url": "/web-dev/static/js/runtime~main.994735d8.js"
+  },
+  {
+    "revision": "5fa1b8f25b4aa8787f70d5b6b7f0b90f",
+    "url": "/web-dev/static/media/icons-16.5fa1b8f2.ttf"
+  },
+  {
+    "revision": "6fdb722c94434811f89a68eafc4531f8",
+    "url": "/web-dev/static/media/icons-16.6fdb722c.eot"
+  },
+  {
+    "revision": "cd1a26696ebf17a89545a3f9067d7028",
+    "url": "/web-dev/static/media/icons-16.cd1a2669.woff"
+  },
+  {
+    "revision": "14279fa7b78ac2230a015925b603a1f7",
+    "url": "/web-dev/static/media/icons-20.14279fa7.eot"
+  },
+  {
+    "revision": "657723dc996cf0ae6b4c6110f5d4ddab",
+    "url": "/web-dev/static/media/icons-20.657723dc.woff"
+  },
+  {
+    "revision": "90247cfdde1bd7b76d804a797d5dec56",
+    "url": "/web-dev/static/media/icons-20.90247cfd.ttf"
+  }
+]);
